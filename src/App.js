2c28d7c2971718f7c150e385dab93b22ae75b245
Timeo Williams <timeo@outsideanalytics.com>
2021-11-03 11:38:51 -0400

    Added custom Astro Web Components to react-table

--- src/App.js
+++ src/App.js
@@ -8,198 +8,207 @@ import React from 'react';
 
 function App() {
 
-    //First, let's create the the array of objects to be displayed. 
-    const rowDef = [];
-    for (let i = 0; i < 1000; i += 1) {
-        rowDef[i] = createEvents();
-        console.log('Event created is', rowDef[i])
-    }
-    function fuzzyTextFilterFn(rows, id, filterValue) {
-        return matchSorter(rows, filterValue, { keys: [row => row.values[id]] })
-    }
+  //First, let's create the the array of objects to be displayed. 
+  const rowDef = [];
+  for (let i = 0; i < 1000; i += 1) {
+    rowDef[i] = createEvents();
+    console.log('Event created is', rowDef[i])
+  }
+  function fuzzyTextFilterFn(rows, id, filterValue) {
+    return matchSorter(rows, filterValue, { keys: [row => row.values[id]] })
+  }
 
-    // Let the table remove the filter if the string is empty
-    fuzzyTextFilterFn.autoRemove = val => !val
-
-
-    const defaultColumn = React.useMemo(
-        () => ({
-            // Let's set up our default Filter UI
-            Filter: DefaultColumnFilter,
-        }),
-        []
-    )
-
-    const filterTypes = React.useMemo(
-        () => ({
-            fuzzyText: fuzzyTextFilterFn,
-
-            text: (rows, id, filterValue) => {
-                return rows.filter(row => {
-                    const rowValue = row.values[id]
-                    return rowValue !== undefined ? String(rowValue)
-                        .toLowerCase()
-                        .startsWith(String(filterValue).toLowerCase())
-                        : true
-                })
-            },
-        }), []
-    )
-
-    const data = React.useMemo(
-        () => rowDef, []
-    )
-    const Columns = React.useMemo(
-        () => [
-
-            {
-                Header: 'Track ID',
-                accessor: 'track-id',
-                filter: 'fuzzyText',
-                minWidth: 150,
-            },
-            {
-                Header: 'Status',
-                accessor: 'status',
-                Filter: SelectColumnFilter,
-                filter: 'includes',
-                minWidth: 150,
-            },
-            {
-                Header: 'Hits',
-                accessor: 'hits',
-                Filter: SliderColumnFilter,
-                filter: 'equals',
-                minWidth: 150,
-            },
-            {
-                Header: 'First Detect',
-                accessor: 'first-detect',
-                Filter: '',
-                minWidth: 150,
-
-            }, {
-                Header: 'Duration',
-                accessor: 'duration',
-                Filter: NumberRangeColumnFilter,
-                filter: 'between',
-            }, {
-                Header: 'Location',
-                accessor: 'location',
-                Filter: '',
-                minWidth: 150,
-            }
-        ], []
-    )
-
-    const tableInstance = useTable(
-        {
-            columns: Columns,
-            data,
-            filterTypes,
-            defaultColumn
-        },
-        useFilters,
-        useSortBy
-    )
-    const spring = React.useMemo(
-        () => ({
-          type: 'spring',
-          damping: 50,
-          stiffness: 100,
-        }),
-        []
-      )
+  // Let the table remove the filter if the string is empty
+  fuzzyTextFilterFn.autoRemove = val => !val
 
 
-    const {
-        getTableProps,
-        getTableBodyProps,
-        headerGroups,
-        rows,
-        prepareRow,
-    } = tableInstance
+  const defaultColumn = React.useMemo(
+    () => ({
+      // Let's set up our default Filter UI
+      Filter: DefaultColumnFilter,
+    }),
+    []
+  )
 
-    // console.log('Rows are', Rows)
+  const filterTypes = React.useMemo(
+    () => ({
+      fuzzyText: fuzzyTextFilterFn,
 
-    return (
+      text: (rows, id, filterValue) => {
+        return rows.filter(row => {
+          const rowValue = row.values[id]
+          return rowValue !== undefined ? String(rowValue)
+            .toLowerCase()
+            .startsWith(String(filterValue).toLowerCase())
+            : true
+        })
+      },
+    }), []
+  )
 
-        <table {...getTableProps()} style={{ border: 'solid 1px blue' }}>
+  const data = React.useMemo(
+    () => rowDef, []
+  )
+  const Columns = React.useMemo(
+    () => [
 
-            <thead>
+      {
+        Header: 'Track ID',
+        accessor: 'track-id',
+        filter: 'fuzzyText',
+        minWidth: 150,
+      },
+      {
+        Header: 'Status',
+        accessor: 'status',
+        Filter: SelectColumnFilter,
+        filter: 'includes',
+        minWidth: 150,
+      },
+      {
+        Header: 'Hits',
+        accessor: 'hits',
+        Filter: SliderColumnFilter,
+        filter: 'equals',
+        minWidth: 150,
+      },
+      {
+        Header: 'First Detect',
+        accessor: 'first-detect',
+        Filter: '',
+        minWidth: 150,
 
-                {headerGroups.map(headerGroup => (
+      }, {
+        Header: 'Duration',
+        accessor: 'duration',
+        Filter: NumberRangeColumnFilter,
+        filter: 'between',
+      }, {
+        Header: 'Location',
+        accessor: 'location',
+        Filter: '',
+        minWidth: 150,
+      }
+    ], []
+  )
 
-                    <tr {...headerGroup.getHeaderGroupProps()}>
+  const tableInstance = useTable(
+    {
+      columns: Columns,
+      data,
+      filterTypes,
+      defaultColumn
+    },
+    useFilters,
+    useSortBy
+  )
+  const spring = React.useMemo(
+    () => ({
+      type: 'spring',
+      damping: 50,
+      stiffness: 100,
+    }),
+    []
+  )
 
-                        {headerGroup.headers.map(column => (
 
-                            <motion.th
-                            {...column.getHeaderProps({
-                              layoutTransition: spring,
-                              style: {
-                                minWidth: column.minWidth,
-                              },
-                            })}
-                          >
-                            <div {...column.getSortByToggleProps()}>
-                              {column.render('Header')}
-                              <span>
-                                  {/* Need to find a more Astro themed up & Down bar */}
-                                {column.isSorted
-                                  ? column.isSortedDesc
-                                    ? ' 🔽'
-                                    : ' 🔼'
-                                  : ''}
-                              </span>
-                            </div>
-                            <div>{column.canFilter ? column.render('Filter') : null}</div>
-                          </motion.th>
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = tableInstance
 
-                        ))}
+  // console.log('Rows are', Rows)
 
-                    </tr>
+  return (
 
-                ))}
+    <rux-table {...getTableProps()}>
 
-            </thead>
+      <rux-table-header>
 
-            <tbody {...getTableBodyProps()}>
-            <AnimatePresence>
-                {rows.map(row => {
-                    //This function is responsible for lazily preparing a row for rendering. Any row that you intend to render in your table needs to be passed to this function before every render.
-                    prepareRow(row)
+        {headerGroups.map(headerGroup => (
+
+          <rux-table-header-row {...headerGroup.getHeaderGroupProps()}>
+
+            {headerGroup.headers.map(column => (
+
+              <motion.th
+                {...column.getHeaderProps({
+                  layoutTransition: spring,
+                  style: {
+                    minWidth: column.minWidth,
+                  },
+                })}
+              >
+                <rux-table-header-cell {...column.getSortByToggleProps()}>
+                  {column.render('Header')}
+                  <span>
+                    {/* Need to find a more Astro themed up & Down bar */}
+                    {column.isSorted
+                      ? column.isSortedDesc
+                        ? ' 🔽'
+                        : ' 🔼'
+                      : ''}
+                  </span>
+                </rux-table-header-cell>
+                <div>{column.canFilter ? column.render('Filter') : null}</div>
+              </motion.th>
+
+            ))}
+
+          </rux-table-header-row>
+
+        ))}
+
+      </rux-table-header>
+
+      <rux-table-body {...getTableBodyProps()}>
+        <AnimatePresence>
+          {rows.map(row => {
+            //This function is responsible for lazily preparing a row for rendering. Any row that you intend to render in your table needs to be passed to this function before every render.
+            prepareRow(row)
+
+            return (
+
+              <motion.tr {...row.getRowProps({
+                layoutTransition: spring,
+                exit: { opacity: 0, maxHeight: 0 },
+              })}>
+                <rux-table-row >
+                  {row.cells.map(cell => {
 
                     return (
 
-                        <motion.tr {...row.getRowProps({
-                            layoutTransition: spring,
-                            exit: { opacity: 0, maxHeight: 0 },
-                          })}>
-
-                            {row.cells.map(cell => {
-
-                                return (
-
-                                    <motion.td
-                                    {...cell.getCellProps({
-                                      layoutTransition: spring,
-                                    })}
-                                  >
-                                    {cell.render('Cell')}
-                                  </motion.td>
-                                )
-                              })}
-                            </motion.tr>
-                          )
+                      <motion.td
+                        {...cell.getCellProps({
+                          layoutTransition: spring,
+                          style: {
+                         minWidth: 180,
+                  },
                         })}
-                      </AnimatePresence>
+                      >
+                        <rux-table-cell>
+                          {cell.render('Cell')}
+                        </rux-table-cell>
 
-            </tbody>
+                      </motion.td>
 
-        </table>
+                    )
+                  })}
+                </rux-table-row>
+                
+              </motion.tr>
+                         
+          )})}
+        </AnimatePresence>
 
-    )
+      </rux-table-body>
+
+    </rux-table>
+
+  )
 
 }
 export default App;
